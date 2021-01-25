ed2e18486f87e6baecb70b8c1769622c38039e43
AndreaCaldiroli <77735945+AndreaCaldiroli@users.noreply.github.com>
2021-01-25 12:26:52 +0100

    Add files via upload

---
+++ Hydro_ioniz.f90
@@ -0,0 +1,235 @@
+      program Hydro_ioniz
+
+      use global_parameters
+      use Read_input
+      use Initialization
+      use Conversion
+      use Evaluate_ioniz_eq
+      use Reconstruction_step
+      use RK_integration
+      use BC_Apply
+      use output_write
+      
+      implicit none
+      
+
+      ! Integers variables
+      integer :: k,j
+      
+      ! Timing variables
+      integer :: n_hrs 
+      integer :: n_min 
+      real*8  :: start, finish 
+      real*8  :: exec_time
+      real*8  :: n_sec
+      
+      ! Momentum variables
+      real*8 :: mom_max,mom_min
+      real*8, dimension(1-Ng:N+Ng) :: mom
+
+      ! Maximum eigenvalue      
+      real*8 :: alpha
+
+      ! Temporal step
+      real*8 :: dt
+ 
+      ! Vectors of thermodynamical variables
+      real*8, dimension(1-Ng:N+Ng) :: rho,v,E,p,T,cs
+      real*8, dimension(1-Ng:N+Ng) :: heat,cool
+      real*8, dimension(1-Ng:N+Ng) :: eta 
+      real*8, dimension(1-Ng:N+Ng) :: nhi,nhii 
+      real*8, dimension(1-Ng:N+Ng) :: nhei,nheii,nheiii 
+      real*8, dimension(1-Ng:N+Ng) :: ne,n_tot  
+      real*8, dimension(1-Ng:N+Ng,5) :: f_sp
+       
+      ! Conservative and primitive vectors
+      real*8, dimension(1-Ng:N+Ng,3) :: u,u1,u2 
+      real*8, dimension(1-Ng:N+Ng,3) :: W,WL,WR
+          
+      ! Flux and source vectors
+      real*8, dimension(1-Ng:N+Ng,3) :: dF,S
+      
+      
+      !------------------------------------------------!
+      
+      ! Read planetary parameters from file
+      call input_read
+      
+      !------------------------------------------------!
+      
+      ! Initialize simulations
+      call init(W,u,f_sp)     
+            
+      !------------------------------------------------!
+      
+      !---- Start computation ----!
+      
+      ! Get starting time
+      start = omp_get_wtime()
+      
+      !------ Main temporal loop ------!
+      
+      do while(du.ge.du_th.and.count.le.1000)
+
+            !---- Time step evaluation ----! 
+            
+            ! Extract phisical variables
+            rho = W(:,1)
+            v   = W(:,2)
+            p   = W(:,3)
+            
+            ! Evaluate sound speed
+            cs = sqrt(g*p/rho)
+            
+            ! Maximum eigenvalue
+            alpha = maxval(abs(v) + cs)
+
+            ! Evaluate time step according to CFL condition
+            dt = CFL*(r(2-Ng)-r(1-Ng))/alpha            
+            
+            !-------------------------------------------------!
+            
+            !--- Thermodynamic evolution ---!
+            
+            ! FIRST RK STEP
+            
+            ! Reconstruct u+_{j+1/2}, u-_{j+1/2}
+            call Reconstruct(dt,u,WL,WR) 
+            
+            ! Evaluate flux difference and source terms
+            call RK_rhs(u,WL,WR,alpha,dF,S)
+            
+            u1 = u - dt*(dF - S)
+             
+            ! Apply boundary conditions
+            call Apply_BC(u1,u1)
+                      
+            !----------------------------
+            
+            ! SECOND RK STEP
+
+            ! Reconstruct u+_{j+1/2}, u-_{j+1/2}
+            call Reconstruct(dt,u1,WL,WR) 
+
+            ! Evaluate flux difference and source terms
+            call RK_rhs(u1,WL,WR,alpha,dF,S)
+                    
+            ! Advance in time
+            u2 = (3.0*u + u1 - dt*(dF - S))/4.0
+
+            ! Apply boundary conditions            
+            call Apply_BC(u2,u2)
+          
+            
+            !----------------------------
+            
+            ! THIRD RK STEP
+
+            ! Reconstruct u+_{j+1/2}, u-_{j+1/2}
+            call Reconstruct(dt,u2,WL,WR) 
+
+            ! Evaluate flux difference and source terms
+            call RK_rhs(u,WL,WR,alpha,dF,S)
+                  
+            ! Advance in time
+            u = (u + 2.0*(u2 - dt*(dF - S)))/3.0
+
+            ! Apply boundary conditions
+            call Apply_BC(u,u)
+ 		
+ 		!---------------------------------------------------!
+ 		
+ 		! Evaluate ionization equilibrium condition
+ 		call eval_ioniz_eq(u,f_sp,u,f_sp,eta,heat,cool)       
+            
+  	      !---------------------------------------------------!
+  	      
+  	      ! Evolve solution in time due to source terms
+            !     using a forward Euler step
+            
+            u(:,3) = u(:,3) + dt*(heat - cool)
+
+		call Apply_BC(u,u)
+
+            !----------------------------------------------------!
+            
+            ! Convert to physical variables and exctract profiles
+            call U_to_W(u,W)
+            rho = W(:,1)
+            v   = W(:,2)
+            p   = W(:,3)
+            E   = u(:,3)
+            
+            ! Evaluate ionized densities and electron density
+  	      nhi    = rho*f_sp(:,1)
+		nhii   = rho*f_sp(:,2)
+		nhei   = rho*f_sp(:,3)
+		nheii  = rho*f_sp(:,4)
+		nheiii = rho*f_sp(:,5)
+  	      ne     = nhii + nheii +2.0*nheiii
+  	      
+  	      ! Total number density
+  	      n_tot = nhi + nhii + nhei + nheii + nheiii  
+  	      
+  	      ! Temperature profile
+  	      T = p/(n_tot + ne)
+ 	      		
+		! Evaluate momentum
+		mom = rho*v*r*r
+		            
+            !---------------------------------------------------!
+        
+            !--- Loop counters and escape condition ---!
+            
+            ! Update counter
+            count = count + 1
+            
+            ! Maximum and minimum value for momentum
+            mom_max = maxval(abs(mom(j_min:N)))
+            mom_min = minval(abs(mom(j_min:N)))
+
+            ! Evaluate relative momentum variation
+            du = abs((mom_max-mom_min)/mom_min)
+                       
+            ! Write to standard output
+            write(*,*) count,du
+            
+            !---------------------------------------------------!
+            
+            !--- Write to files every 100th iteration---! 
+            
+            if (mod(count,100).eq.2) then
+                  
+                  ! Write thermodynamic and ionization profiles
+                  call write_output(rho,v,p,T,heat,cool,eta,    &
+                                    nhi,nhii,nhei,nheii,nheiii)
+                                   
+            endif     
+            
+            !---------------------------------------------------!
+            
+      ! End of temporal while loop      
+      enddo
+
+      !---------------------------------------------------!
+      
+      ! Get CPU time
+      finish = omp_get_wtime()
+      
+      ! Write final execution time
+      exec_time = finish - start
+      n_hrs = nint(exec_time/3600.0)
+      n_min = nint((exec_time - 3600.0*n_hrs)/60.0)
+      n_sec = exec_time - 3600.0*n_hrs - 60.0*n_min
+
+      
+      write(*,100) 'Execution Time = ', n_hrs,' h ', &
+                                        n_min,' m ', &
+                                        n_sec,' s'
+100   format  (A17,I2,A3,I2,A3,F8.5,A2) 
+      
+      !---------------------------------------------------!
+      
+      
+      ! End of program
+      end program Hydro_ioniz
---
+++ run_program.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+# Executable bash script to tun the full code        #
+# v.2.0 22-01-2021
+
+############# Directories ###############
+
+# Current directory
+PWD=$(pwd)
+
+# Main program directory
+DIR_MAIN="$PWD" 
+
+# Source files directory
+DIR_SRC="$PWD/src" 
+
+# Mod files directory
+DIR_MOD="$DIR_SRC/mod" 
+
+# Utilities directory
+DIR_UTILS="$DIR_SRC/utils"
+
+# Modules directories
+DIR_FILES="$DIR_SRC/modules/files_IO"
+DIR_FLUX="$DIR_SRC/modules/flux"
+DIR_FUNC="$DIR_SRC/modules/functions"
+DIR_INIT="$DIR_SRC/modules/init"
+DIR_NLSOLVE="$DIR_SRC/modules/nonlinear_system_solver"
+DIR_RAD="$DIR_SRC/modules/radiation"
+DIR_STAT="$DIR_SRC/modules/states"
+DIR_TIME="$DIR_SRC/modules/time_step"
+
+############# Cleaning old files ###############
+
+# Clean old executable 
+rm -f $DIR_MAIN/*.x
+
+# Clean .mod files
+rm -f $DIR_MOD/*.mod
+
+########### Call python interface to create input file ############
+
+python3 -W ignore "$DIR_UTILS/create_input.py"
+
+########### Check input parameters ############
+
+INPUT_FILE="$DIR_MAIN/input.inp"
+
+# Check if input parameters file exists
+echo "Searching for the input parameters file..."
+if [ -f "$INPUT_FILE" ]; then
+
+      echo "Input file found. Proceeding..."
+
+else  # Abort if no input.inp exists
+
+      echo "Missing input parameter file."
+      echo "Please create an input.inp file"
+      exit 1
+fi
+
+
+############# Executing fortran file ###############
+
+echo "
+----- STARTING SIMULATION -----
+"
+
+# Initialize string of commands with ignored files. 
+# This ensures that these files are compiled first 
+str=" gfortran -J"$DIR_MOD" -I"$DIR_MOD" -fopenmp -g -fbacktrace \
+      $DIR_INIT/parameters.f90\
+      $DIR_FILES/input_read.f90\
+      $DIR_FILES/load_IC.f90\
+      $DIR_FILES/write_output.f90\
+      $DIR_FUNC/grav_field.f90\
+      $DIR_FUNC/sigma.f90\
+      $DIR_FUNC/sigma_HeI.f90\
+      $DIR_FUNC/UW_conversions.f90\
+      $DIR_NLSOLVE/dogleg.f90\
+      $DIR_NLSOLVE/enorm.f90\
+      $DIR_NLSOLVE/hybrd1.f90\
+      $DIR_NLSOLVE/qform.f90\
+      $DIR_NLSOLVE/r1mpyq.f90\
+      $DIR_NLSOLVE/System_HeH.f90\
+      $DIR_NLSOLVE/dpmpar.f90\
+      $DIR_NLSOLVE/fdjac1.f90\
+      $DIR_NLSOLVE/hybrd.f90\
+      $DIR_NLSOLVE/qrfac.f90\
+      $DIR_NLSOLVE/r1updt.f90\
+      $DIR_NLSOLVE/System_H.f90\
+      $DIR_RAD/J_inc.f90\
+      $DIR_RAD/Cool_coeff.f90\
+      $DIR_RAD/ionization_equilibrium.f90\
+      $DIR_RAD/eval_ioniz_eq.f90\
+      $DIR_STAT/Apply_BC.f90\
+      $DIR_STAT/PLM_rec.f90\
+      $DIR_STAT/WENO3_rec.f90\
+      $DIR_STAT/Source.f90\
+      $DIR_STAT/Reconstruction.f90\
+      $DIR_FLUX/speed_estimate_HLLC.f90\
+      $DIR_FLUX/speed_estimate_ROE.f90\
+      $DIR_FLUX/Num_Fluxes.f90\
+      $DIR_TIME/RK_rhs.f90\
+      $DIR_INIT/define_grid.f90\
+      $DIR_INIT/set_energy_vectors.f90\
+      $DIR_INIT/set_IC.f90\
+      $DIR_INIT/init.f90"
+      
+# Add main file to execution
+str="$str $DIR_MAIN/Hydro_ioniz.f90"
+
+# Add output file to string of execution
+compile="$str -o $DIR_MAIN/Hydro_ioniz.x"
+
+# Compile program and modules
+eval $compile
+
+# Exectution string
+exec_str="$DIR_MAIN/Hydro_ioniz.x"
+
+# Exectute code
+eval $exec_str
+
+# Print when execution is over
+echo "
+----- FORTRAN EVALUATION DONE -----"
+
+############# Python plots ###############
+
+# Run python_plots
+#python3 -W ignore python_plots.py 
+
+
+
