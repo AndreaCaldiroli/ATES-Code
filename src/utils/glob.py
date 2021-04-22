b9e80e9079c7897a82fc09ca6bf0a38ecbc1f69c
AndreaCaldiroli <77735945+AndreaCaldiroli@users.noreply.github.com>
2021-04-22 10:56:53 +0200

    Add files via upload

---
+++ src/utils/glob.py
@@ -0,0 +1,27 @@
+# Definition of common variables
+
+def init():
+      global ent_list, Cbox_list, note
+      global Rp,Mp,T0,LX,LEUV,a,Ms,pl_names,appx_meth
+      global b0_lbl_var,lgphi_lbl_var,rho_lbl_var,resc_lbl_var
+      global EUV_var,IC_var,write_ans
+      
+      # Widget list
+      ent_list  = []
+      Cbox_list = []
+      
+      # List of physical paramters
+      pl_names = []
+      note = []
+      Rp   = []
+      Mp   = []
+      T0   = []
+      LX   = []
+      LEUV = []
+      a    = []
+      Ms   = []
+      appx_meth = []
+      
+      
+      
+      
