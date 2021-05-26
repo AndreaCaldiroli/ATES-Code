006e484e1f38bbebd478c5f7a9e49aaa8d330d5e
AndreaCaldiroli <77735945+AndreaCaldiroli@users.noreply.github.com>
2021-05-26 15:30:28 +0200

    Update python_plots.py

--- python_plots.py
+++ python_plots.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 
 
@@ -56,7 +57,7 @@ fheii  = nheii[:]/nhe[:]    # HeII fraction
 fheiii = nheiii[:]/nhe[:]   # HeIII fraction
 
 # Spherical momentum
-mom   = 4.*np.pi*v[:]*rho[:]*r[:]**2.*v0*R0**2.            # Our momentum
+mom   = 4.*np.pi*v[:]*rho[:]*r[:]**2.*v0*R0**2.            
 
 # Correct mass flux for the current 3D approximation method
 mom_out = mom[-20]
@@ -71,11 +72,12 @@ if appx_mth.strip() == "Mdot/4":
 #------- Hydro plot -------#
 
 fig, ax = plt.subplots(2,4)
+plt.suptitle('Before post-processing',fontsize = 15,fontweight = 'bold')
 fig.set_size_inches(11, 7)
 fig.subplots_adjust(left   = 0.05,
                     bottom = 0.08,
                     right  = 0.97,
-                    top    = 0.94,
+                    top    = 0.89,
                     hspace = 0.30,
                     wspace = 0.31)  
 
@@ -143,10 +145,137 @@ ax[1,3].set_xlabel('r/R$_P$')
 ax[1,3].legend(loc = 'best')
 
 
+#------------------------------------------------------------
+
+# Plot post processed data if the files are there
+
+# Get file paths
+cdir      = os.getcwd()
+adv_hydro = cdir + "/output/Hydro_ioniz_adv.txt"
+adv_ioniz = cdir + "/output/Ion_species_adv.txt"
+
+# Make second figure if _adv files are there
+if os.path.isfile(adv_hydro ) and os.path.isfile(adv_ioniz):
+
+	
+	# Hydro profiles
+	r,rho,v,p,T,heat,cool,eta = \
+		np.loadtxt('./output/Hydro_ioniz_adv.txt',unpack = True)
+	rho = rho*mu
+
+	# Load our ionization profiles
+	r,nhi,nhii,nhei,nheii,nheiii = \
+		np.loadtxt('./output/Ion_species_adv.txt',unpack = True)
+
+
+	#--------------------------------------------------
+
+
+	# Ion densities and fractions
+	nh  = nhi[:] + nhii[:]                    # Total hydrogen density
+	nhe = nhei[:] + nheii[:] + nheiii[:]      # Total helium density
+	ne  = nhii[:] + nheii[:] + 2.*nheiii[:]   # Free electron density
+	fhi    = nhi[:]/nh[:]       # HI fraction
+	fhii   = nhii[:]/nh[:]      # HII fraction
+	fhei   = nhei[:]/nhe[:]     # HeI fraction
+	fheii  = nheii[:]/nhe[:]    # HeII fraction
+	fheiii = nheiii[:]/nhe[:]   # HeIII fraction
+
+	# Spherical momentum
+	mom   = 4.*np.pi*v[:]*rho[:]*r[:]**2.*v0*R0**2.            
+
+	# Correct mass flux for the current 3D approximation method
+	mom_out = mom[-20]
+	if appx_mth.strip() == "Rate/2 + Mdot/2":
+		mom_out = 0.5*mom[-20]
+	if appx_mth.strip() == "Mdot/4":
+		mom_out = 0.25*mom[-20]
+
+
+	#----------------------------------------------------#
+
+	#------- Hydro plot -------#
+
+	fig, ax = plt.subplots(2,4)
+	plt.suptitle('After post-processing',fontsize = 15,fontweight = 'bold')
+	fig.set_size_inches(11, 7)
+	fig.subplots_adjust(left   = 0.05,
+		              bottom = 0.08,
+		              right  = 0.97,
+		              top    = 0.89,
+		              hspace = 0.30,
+		              wspace = 0.31)   
+
+	# Density
+	ax[0,0].semilogy(r,rho)
+	ax[0,0].set_xlim([r[0],r[-1]])
+	ax[0,0].set_title('Density [g cm$^{-3}$]', fontdict={'weight':'bold'})
+	ax[0,0].set_xlabel('r/R$_P$')
+
+
+	# Velocity
+	ax[0,1].semilogy(r,v*v0*1e-5)
+	ax[0,1].set_xlim([r[0],r[-1]])
+	ax[0,1].set_ylim([1.e-3,1.2*v.max()*v0*1e-5])
+	ax[0,1].set_title('Velocity [km s$^{-1}$]', fontdict={'weight':'bold'})
+	ax[0,1].set_xlabel('r/R$_P$')
+
+	# Pressure
+	ax[0,2].semilogy(r,p)
+	ax[0,2].set_xlim([r[0],r[-1]])
+	ax[0,2].set_title('Pressure [erg cm$^{-3}$]', fontdict={'weight':'bold'})
+	ax[0,2].set_xlabel('r/R$_P$')
+
+	# Temperature
+	ax[0,3].plot(r,T)
+	ax[0,3].set_xlim([r[0],r[-1]])
+	ax[0,3].set_title('Temperature [K]', fontdict={'weight':'bold'})
+	ax[0,3].set_xlabel('r/R$_P$')
+
+	# Momentum
+	ax[1,0].plot(r,np.log10(mom))
+	ax[1,0].set_xlim([r[0],r[-1]])
+	ax[1,0].set_title('Log10 Momentum [g s$^{-1}$]', fontdict={'weight':'bold'})
+	ax[1,0].set_xlabel('r/R$_P$')
+
+	# Ionization densities
+	ax[1,1].semilogy(r,nhi,label = '$n_{HI}$')
+	ax[1,1].semilogy(r,nhii,label = '$n_{HII}$')
+	ax[1,1].semilogy(r,nhei,label = '$n_{HeI}$')
+	ax[1,1].semilogy(r,nheii,label = '$n_{HeII}$')
+	ax[1,1].semilogy(r,nheiii,label = '$n_{HeIII}$')
+	ax[1,1].set_title('Ion densities [cm$^{-3}$]', fontdict={'weight':'bold'})
+	ax[1,1].set_xlim([r[0],r[-1]])
+	ax[1,1].set_xlabel('r/R$_P$')
+	ax[1,1].legend(loc = 'upper right')
+
+
+	# H fractions
+	ax[1,2].plot(r,fhi,label = '$f_{HI}$')
+	ax[1,2].plot(r,fhii,label = '$f_{HII}$')
+	ax[1,2].set_title('H fractions', fontdict={'weight':'bold'})
+	ax[1,2].set_xlim([r[0],r[-1]])
+	ax[1,2].set_ylim([0,1])
+	ax[1,2].set_xlabel('r/R$_P$')
+	ax[1,2].legend(loc = 'best')
+
+	# He fractions
+	ax[1,3].plot(r,fhei,label = '$f_{HeI}$')
+	ax[1,3].plot(r,fheii,label = '$f_{HeII}$')
+	ax[1,3].plot(r,fheiii,label = '$f_{HeIII}$')
+	ax[1,3].set_title('He fractions', fontdict={'weight':'bold'})
+	ax[1,3].set_xlim([r[0],r[-1]])
+	ax[1,3].set_ylim([0,1])
+	ax[1,3].set_xlabel('r/R$_P$')
+	ax[1,3].legend(loc = 'best')
+
+
+
+
+
 # Print the mass loss rate
 print("2D approximation method: ", appx_mth.strip())
 print("Log10 of mass-loss-rate = ", np.log10(mom_out))
 
 plt.show()
-
 
