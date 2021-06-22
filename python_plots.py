0c08bf35ea1ec8565860aa144059d655886f396a
AndreaCaldiroli <77735945+AndreaCaldiroli@users.noreply.github.com>
2021-06-22 10:51:31 +0200

    Update python_plots.py

--- python_plots.py
+++ python_plots.py
@@ -184,7 +184,6 @@ if os.path.isfile(adv_hydro ) and os.path.isfile(adv_ioniz):
 	# Ion densities and fractions
 	nh  = nhi[:] + nhii[:]                    # Total hydrogen density
 	nhe = nhei[:] + nheii[:] + nheiii[:]      # Total helium density
-	ne  = nhii[:] + nheii[:] + 2.*nheiii[:]   # Free electron density
 	fhi    = nhi[:]/nh[:]       # HI fraction
 	fhii   = nhii[:]/nh[:]      # HII fraction
 	fhei   = nhei[:]/nhe[:]     # HeI fraction
@@ -192,82 +191,30 @@ if os.path.isfile(adv_hydro ) and os.path.isfile(adv_ioniz):
 	fheiii = nheiii[:]/nhe[:]   # HeIII fraction
 
 	#----------------------------------------------------#
-
-	#------- Hydro plot -------#
-
-	fig, ax = plt.subplots(2,4)
-	plt.suptitle('After post-processing',fontsize = 15,fontweight = 'bold')
-	fig.set_size_inches(11, 7)
-	fig.subplots_adjust(left   = 0.05,
-		              bottom = 0.08,
-		              right  = 0.97,
-		              top    = 0.89,
-		              hspace = 0.30,
-		              wspace = 0.31)   
-
-	# Density
-	ax[0,0].semilogy(r,rho)
-	ax[0,0].set_xlim([r[0],r[-1]])
-	ax[0,0].set_title('Density [g cm$^{-3}$]', fontdict={'weight':'bold'})
-	ax[0,0].set_xlabel('r/R$_P$')
-
-
-	# Velocity
-	ax[0,1].semilogy(r,v*v0*1e-5)
-	ax[0,1].set_xlim([r[0],r[-1]])
-	ax[0,1].set_ylim([1.e-3,vlim])
-	ax[0,1].set_title('Velocity [km s$^{-1}$]', fontdict={'weight':'bold'})
-	ax[0,1].set_xlabel('r/R$_P$')
-
+	
+	# Add Post-processed profiles
+	
 	# Pressure
-	ax[0,2].semilogy(r,p)
-	ax[0,2].set_xlim([r[0],r[-1]])
-	ax[0,2].set_title('Pressure [erg cm$^{-3}$]', fontdict={'weight':'bold'})
-	ax[0,2].set_xlabel('r/R$_P$')
+	ax[0,2].semilogy(r,p,'--')
 
 	# Temperature
-	ax[0,3].plot(r,T)
-	ax[0,3].set_xlim([r[0],r[-1]])
-	ax[0,3].set_title('Temperature [K]', fontdict={'weight':'bold'})
-	ax[0,3].set_xlabel('r/R$_P$')
-
-	# Momentum
-	ax[1,0].plot(r,lgmom)
-	ax[1,0].set_xlim([r[0],r[-1]])
-	ax[1,0].set_title('Log10 Momentum [g s$^{-1}$]', fontdict={'weight':'bold'})
-	ax[1,0].set_xlabel('r/R$_P$')
-	ax[1,0].set_ylim([7,13])
-
+	ax[0,3].plot(r,T,'--')
+	
 	# Ionization densities
-	ax[1,1].semilogy(r,nhi,label = '$n_{HI}$')
-	ax[1,1].semilogy(r,nhii,label = '$n_{HII}$')
-	ax[1,1].semilogy(r,nhei,label = '$n_{HeI}$')
-	ax[1,1].semilogy(r,nheii,label = '$n_{HeII}$')
-	ax[1,1].semilogy(r,nheiii,label = '$n_{HeIII}$')
-	ax[1,1].set_title('Ion densities [cm$^{-3}$]', fontdict={'weight':'bold'})
-	ax[1,1].set_xlim([r[0],r[-1]])
-	ax[1,1].set_xlabel('r/R$_P$')
-	ax[1,1].legend(loc = 'upper right')
-
+	ax[1,1].semilogy(r,nhi,'--',label = '$n_{HI}$',color = '#1f77b4')
+	ax[1,1].semilogy(r,nhii,'--',label = '$n_{HII}$',color = '#ff7f0e')
+	ax[1,1].semilogy(r,nhei,'--',label = '$n_{HeI}$',color = '#2ca02c')
+	ax[1,1].semilogy(r,nheii,'--',label = '$n_{HeII}$',color = '#d62728')
+	ax[1,1].semilogy(r,nheiii,'--',label = '$n_{HeIII}$',color = '#9467bd')
 
 	# H fractions
-	ax[1,2].plot(r,fhi,label = '$f_{HI}$')
-	ax[1,2].plot(r,fhii,label = '$f_{HII}$')
-	ax[1,2].set_title('H fractions', fontdict={'weight':'bold'})
-	ax[1,2].set_xlim([r[0],r[-1]])
-	ax[1,2].set_ylim([0,1])
-	ax[1,2].set_xlabel('r/R$_P$')
-	ax[1,2].legend(loc = 'best')
+	ax[1,2].plot(r,fhi,'--',label = '$f_{HI}$',color = '#1f77b4')
+	ax[1,2].plot(r,fhii,'--',label = '$f_{HII}$',color = '#ff7f0e')
 
 	# He fractions
-	ax[1,3].plot(r,fhei,label = '$f_{HeI}$')
-	ax[1,3].plot(r,fheii,label = '$f_{HeII}$')
-	ax[1,3].plot(r,fheiii,label = '$f_{HeIII}$')
-	ax[1,3].set_title('He fractions', fontdict={'weight':'bold'})
-	ax[1,3].set_xlim([r[0],r[-1]])
-	ax[1,3].set_ylim([0,1])
-	ax[1,3].set_xlabel('r/R$_P$')
-	ax[1,3].legend(loc = 'best')
+	ax[1,3].plot(r,fhei,'--',label = '$f_{HeI}$',color = '#1f77b4')
+	ax[1,3].plot(r,fheii,'--',label = '$f_{HeII}$',color = '#ff7f0e')
+	ax[1,3].plot(r,fheiii,'--',label = '$f_{HeIII}$',color = '#2ca02c')
 
 
 
